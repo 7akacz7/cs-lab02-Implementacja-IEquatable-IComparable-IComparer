19d72becba991fac2c7fa29cd66a86e825ccc35f
Andrzej Klimala <andrzej.klimalagit@gmail.com>
2023-03-22 19:40:02 +0100

    1.6

--- Pracownik.cs
+++ Pracownik.cs
@@ -11,6 +11,20 @@ public class Pracownik
         get => _wyn;
         set => _wyn = (value < 0) ? 0 : value;
     }
-    public override string ToString() => $"({Nazwisko}, {DataZatrudnienia:d MMM yyyy}, {Wynagrodzenie} PLN)";
+    public override string ToString()
+        => $"({Nazwisko}, {DataZatrudnienia:d MMM yyyy} ({CzasZatrudnienia}), {Wynagrodzenie} PLN)";
     public int CzasZatrudnienia => (DateTime.Now - DataZatrudnienia).Days / 30;
+    public Pracownik(string nazwisko, DateTime dataPrzyjecia, decimal wynagrodzenie)
+    {
+        Nazwisko = nazwisko;
+        DataZatrudnienia = dataPrzyjecia;
+        Wynagrodzenie = wynagrodzenie;
+    }
+    public Pracownik(string nazwisko, DateTime dataPrzyjecia, decimal wynagrodzenie)
+    {
+        nazwisko = "Anonim";
+        dataPrzyjecia = DateTime.Today();
+        wynagrodzenie = 0;
+    }
+
 }

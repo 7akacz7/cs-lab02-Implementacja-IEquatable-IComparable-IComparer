dbbe48fb00bb740a5ac6325fccd7af9f27e06044
Andrzej Klimala <andrzej.klimalagit@gmail.com>
2023-03-22 19:20:47 +0100

    1.2

--- Pracownik.cs
+++ Pracownik.cs
@@ -3,6 +3,12 @@
 public class Pracownik
 {
 	public string Nazwisko { get; set; }
+	Nazwisko.Trim();
 	public DateTime DataZatrudnienia { get; set; }
-	public decimal Wynagrodzenie { get; set; }
+	if(DataZatrudnienia > DateTime.Today()) throw new ArgumentException();
+    public decimal Wynagrodzenie
+    {
+        get => _wyn;
+        set => _wyn = (value < 0) ? 0 : value;
+    }
 }
